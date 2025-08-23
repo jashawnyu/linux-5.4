aaca0901b160c72b5525ffcf55102612a0e3e726
jashawnyu <jashawnyu@gmail.com>
2025-08-23 11:04:03 +0800

    ubuntu-base

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ! -e vmlinux ]; then 
-make  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
-scripts/kconfig/merge_config.sh -m .config my.config
+#make  ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
 #sed -i '26s/^/extern /' scripts/dtc/dtc-lexer.l
+scripts/kconfig/merge_config.sh -m .config my.config
 fi
 unset https_proxy
 unset http_proxy
 #bear -- make -j8 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- 
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- vmlinux
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 [ $? -eq 0 ] && ./gen_compile_commands.py && sed -i 's/-mabi=lp64//g' compile_commands.json
 
--- qemu-run-directly.sh
+++ qemu-run-directly.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 #qemu-system-aarch64 -machine virt -cpu cortex-a57 -nographic -smp 2 -m 1024 -kernel arch/arm64/boot/Image --append "loglevel=8" --fsdev local,id=share9p,path=$PWD/sharefs,security_model=none -device virtio-9p-device,fsdev=share9p,mount_tag=net9p 
-qemu-system-aarch64 -machine virt -cpu cortex-a57 -nographic -smp 2 -m 1024 -kernel arch/arm64/boot/Image --append "loglevel=8" --fsdev local,id=share9p,path=$PWD/sharefs,security_model=none -device virtio-9p-device,fsdev=share9p,mount_tag=net9p \
-  -netdev type=user,id=eth0 -device virtio-net-device,netdev=eth0
+sudo qemu-system-aarch64 -machine virt -cpu cortex-a57 -nographic -smp 2 -m 1024 -kernel arch/arm64/boot/Image --append "rootwait root=/dev/vda rw console=ttyAMA0 init=/lib/systemd/systemd loglevel=8" --fsdev local,id=share9p,path=$PWD/sharefs,security_model=none -device virtio-9p-device,fsdev=share9p,mount_tag=net9p \
+-drive file=ubuntu-base.ext4,if=none,format=raw,id=hd0 \
+-device virtio-blk-device,drive=hd0
+ # -netdev type=user,id=eth0 -device virtio-net-device,netdev=eth0 
